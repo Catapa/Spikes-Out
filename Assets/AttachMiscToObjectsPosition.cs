40185f8688581ad3ca5c4505419d8eab5883dea8
Cătălin Pap <catalin24pap@gmail.com>
2022-02-28 17:00:00 +0200

    Add files via upload

---
+++ Assets/AttachMiscToObjectsPosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachMiscToObjectsPosition: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject ObjectToFollow;
+    public float smoothing = 0f;
+    public bool DestroyWithObject = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //gameObject.transform.position = ObjectToFollow.transform.position;
+
+        //transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(ObjectToFollow.transform.position.x, ObjectToFollow.transform.position.y, -1), smoothing * Time.deltaTime);
+        DestroyIfObjectDestroyed();
+    }
+
+    void DestroyIfObjectDestroyed()
+    {
+        if (DestroyWithObject)
+        {
+            if(!ObjectToFollow.activeInHierarchy)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/FadeInFadeOut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    float blinkProgress = 0f;
+    float blinkStep = 0.05f;
+    Text blinkingText;
+
+    void Start()
+    {
+        blinkingText = gameObject.GetComponent<Text>();
+    }
+    void Update()
+    {
+        if ((blinkProgress > 1) || (blinkProgress < 0))
+        {
+            blinkStep *= -1f;
+        }
+        blinkProgress += blinkStep;
+        blinkingText.color = Color.Lerp(Color.black, Color.white, blinkProgress);// or whatever color you choose
+
+    }
+}
